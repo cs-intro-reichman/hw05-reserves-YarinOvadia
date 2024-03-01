f97e57f316d9cde9e49f943cf9e6fd9a387e4d45
Yarin Ovadia <yarin.ovadia@post.runi.ac.il>
2024-03-01 16:42:52 +0200

    update

--- GameOfLife.java
+++ GameOfLife.java
@@ -17,7 +17,7 @@ public class GameOfLife {
          */
          
          
-         test3(fileName, 3);
+         //test3(fileName, 3);
          play(fileName);
 
     }
@@ -25,7 +25,7 @@ public class GameOfLife {
     // Reads the data file and prints the initial board.
     public static void test1(String fileName) {
         int[][] board = read(fileName);
-        //print(board);
+        print(board);
     }
         
     // Reads the data file, and runs a test that checks 
